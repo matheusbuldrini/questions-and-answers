7f79b703b84282db605ffc07aeff7dd86c3761a6
matheus <matheus.rbb@hotmail.com>
2019-03-20 20:46:38 -0300

    teste

--- a.py
+++ a.py
@@ -1,1 +1,6 @@
-print("ola python")
+import requests
+
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
