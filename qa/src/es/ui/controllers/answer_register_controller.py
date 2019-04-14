c86046e1c2465378b33041858f18cb3f2180be9b
Fernanda Guimarães <fernandaguimaraes28@gmail.com>
2019-04-14 18:21:32 -0300

    Added more controllres.

---
+++ qa/src/es/ui/controllers/answer_register_controller.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+class AnswerRegister:
+    def __init__(self):
+        """
+
+        """
---
+++ qa/src/es/ui/controllers/question_register_controller.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+class QuestionRegister:
+    def __init__(self):
+        """
+
+        """
