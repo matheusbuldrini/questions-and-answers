0b73fbd71db567f2237da74e93cb3521ba5f2950
matheusbuldrini <38816666+matheusbuldrini@users.noreply.github.com>
2019-04-21 20:04:45 -0300

    Configured database.

---
+++ classes/Database.py
@@ -0,0 +1,31 @@
+#Username: h0X0rG27Cf
+#Password: X5vM8SVTEO
+#Database Name: h0X0rG27Cf
+#Server: remotemysql.com
+#Port: 3306
+
+#Server: sql10.freemysqlhosting.net
+#Name: sql10288957
+#Username: sql10288957
+#Password: jervEuXDVq
+#Port number: 3306
+
+import pymysql
+
+class Database:
+    def __init__(self):
+        host = "remotemysql.com"
+        user = "h0X0rG27Cf"
+        password = "X5vM8SVTEO"
+        db = "h0X0rG27Cf"
+        self.con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.DictCursor, autocommit=True)
+        self.cur = self.con.cursor()
+
+    def list(self, sql):
+        #returns list of elements
+        self.cur.execute(str(sql))
+        return self.cur.fetchall()
+
+    def sql(self, sql):
+        #returns number of affected items
+        return self.cur.execute(str(sql))
--- run.py
+++ run.py
@@ -1,17 +1,25 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
+import classes.Database as Database
 app = Flask(__name__)
 
-@app.route("/")	
+@app.route("/")
 @app.route("/home")
 def home():
 	perguntas = [{'id': 1, 'titulo': 'primeira perg', 'desc': 'bla bla bla'}, {'id': 2, 'titulo': 'segunda perg', 'desc': 'mais bla bla bla'}]
 	return render_template('home.html', perguntas=perguntas)
 
-@app.route("/pergunta/<string:pergunta_id>/")
-def hello(pergunta_id):
+@app.route("/pergunta/<int:pergunta_id>/")
+def pergunta(pergunta_id):
     return render_template('pergunta.html',pergunta_id=pergunta_id)
-	
+
+@app.route("/testdb/")
+def testdb():
+	db = Database.Database()
+	#return str(db.list("SELECT nickname, password FROM user"))
+	return str(db.sql('UPDATE user SET password="827CCB0EEA8A706C4C34A16891F84E7J"'))
+	#return str(db.list('INSERT INTO user VALUES (NULL, "test", "827CCB0EEA8A706C4C34A16891F84E7B")'))
+    #return render_template('usuario.html',usuario_id=usuario_id)
 
 if __name__ == "__main__":
     # Bind to PORT if defined, otherwise default to 5000.
