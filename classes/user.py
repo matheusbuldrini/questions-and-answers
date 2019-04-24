378eff443cd79747f19c18c982f35368f66c3d1e
matheusbuldrini <38816666+matheusbuldrini@users.noreply.github.com>
2019-04-24 00:27:59 -0300

    Adicionado login e pequenas modificacoes.

--- classes/user.py
+++ classes/user.py
@@ -1,12 +1,29 @@
-import classes.Database as Database
+import classes.database as Database
 
 
 class User:
     def __init__(self):
         self.db = Database.Database()
 
-    def select_all(self):
-        return self.db.list('SELECT * FROM user')
+    def _select_all(self):
+        return self.db.get_list('SELECT * FROM user')
 
-    def insert(self, nickname, password):
-        return self.db.sql('INSERT INTO user(nickname, password) VALUES ("' + nickname + '", "' + password + '")')
+    def _select_count_by_email(self, email):
+        return int(self.db.get_list('SELECT COUNT(*) AS COUNT FROM user WHERE email = "' + email + '"')[0]['COUNT'])
+
+    def _select_count_by_email_password(self, email, password):
+        return int(self.db.get_list('SELECT COUNT(*) AS COUNT FROM user WHERE email = "' + email + '" AND password = "' + password + '"')[0]['COUNT'])
+
+    def _insert(self, fullname, email, password):
+        return self.db.sql('INSERT INTO user(fullname, email, password) VALUES ("' + fullname + '", "' + email + '", "' + password + '")')
+
+    def validate_register(self, fullname, email, password):
+        if self._select_count_by_email(email) == 0:
+            if self._insert(fullname, email, password) == 1:
+                return True
+        return False
+
+    def validate_login(self, email, password):
+        if self._select_count_by_email_password(email, password) == 1:
+            return True
+        return False
--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
-import classes.Database as Database
-import classes.User as User
+import classes.user as User
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,22 +12,22 @@ def home():
 @app.route("/pergunta/<int:pergunta_id>/")
 def pergunta(pergunta_id):
     return render_template('pergunta.html',pergunta_id=pergunta_id)
-	
+
 @app.route("/cadastro", methods=['GET', 'POST'])
 def cadastro():
     if request.method == 'POST':
         user = User.User()
-        return str(user.insert(request.form['nickname'], request.form['password']))
+        return str(user.validate_register(request.form['fullname'], request.form['email'], request.form['password']))
     else:
         return render_template('cadastro.html')
 
-@app.route("/testdb/")
-def testdb():
-	db = Database.Database()
-	#return str(db.list("SELECT nickname, password FROM user"))
-	return str(db.sql('UPDATE user SET password="827CCB0EEA8A706C4C34A16891F84E7J"'))
-	#return str(db.list('INSERT INTO user VALUES (NULL, "test", "827CCB0EEA8A706C4C34A16891F84E7B")'))
-    #return render_template('usuario.html',usuario_id=usuario_id)
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.User()
+        return str(user.validate_login(request.form['email'], request.form['password']))
+    else:
+        return render_template('login.html')
 
 if __name__ == "__main__":
     # Bind to PORT if defined, otherwise default to 5000.
